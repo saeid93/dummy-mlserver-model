d638b5c906907be23fbbee6dbfca62e854df6a34
Saeid Ghafouri <saeid.ghafouri@seldon.io>
2023-10-16 17:35:05 +0100

    added minimal model for testing triton input-validation

---
+++ minimal-triton-python-backend/echo_model/1/echo_model.py
@@ -0,0 +1,15 @@
+import triton_python_backend_utils as pb_utils
+
+class EchoModel(pb_utils.TritionPythonModel):
+    def initialize(self, args):
+        # No initialization is needed
+        pass
+
+    def finalize(self):
+        # No finalization is needed
+        pass
+
+    def inference(self, model_input):
+        # Simply echo the input
+        model_output = model_input
+        return pb_utils.InferenceResult(output=model_output)
--- triton-client.py
+++ triton-client.py
@@ -4,7 +4,7 @@ from tritonclient.utils import InferenceServerException
 import math
 
 def test_infer(model_name, input_data):
-    inputs = httpclient.InferInput("INPUT0", list(input_data.shape), "FP16")
+    inputs = httpclient.InferInput("INPUT0", list(input_data.shape) + [1], "FP16")
     # inputs.append(httpclient.InferInput("INPUT0", input_data.shape, "BYTES"))
     inputs.set_data_from_numpy(input_data, binary_data=False)
     inputs._parameters = {"content_type": "np"}
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     # input_data = np.expand_dims(input_data, axis=0)
     # input_data = np.array([[ 1., math.nan,  0.]], dtype=np.float16)
     # input_data = np.array([1, 2, None, 4])
-    input_data = np.array([[ 1., None,  0.]], dtype=np.float16)
+    input_data = np.array([[ 1., 2.,  0.]], dtype=np.float16)
 
     results = test_infer(model_name, input_data)
     # print(results.get_response())
